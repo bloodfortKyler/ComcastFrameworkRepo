24aa3af960957f11e35413fd43d9f73dfb7f54e1
abhisdv <32778204+abhisdv@users.noreply.github.com>
2025-01-04 11:25:35 +0530

    from tl

--- Vtiger_crm/src/test/java/pac1/CreateContactTest.java
+++ Vtiger_crm/src/test/java/pac1/CreateContactTest.java
@@ -1,8 +1,7 @@
 package pac1;
 
 public class CreateContactTest {
-	private void what() {
-		System.out.println("hello message from kylerblood");
-		System.out.println("2nd message");
+	public void hello() {
+		System.out.println("message of team lead");
 	}
 }
