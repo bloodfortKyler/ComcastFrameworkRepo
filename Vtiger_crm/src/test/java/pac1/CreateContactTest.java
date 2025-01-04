8b036a1cb528fed31d042ef922f4e846420db5c7
abhisdv <32778204+abhisdv@users.noreply.github.com>
2025-01-04 13:11:49 +0530

    hound method

--- Vtiger_crm/src/test/java/pac1/CreateContactTest.java
+++ Vtiger_crm/src/test/java/pac1/CreateContactTest.java
@@ -1,7 +1,7 @@
 package pac1;
 
 public class CreateContactTest {
-	public void hello() {
+	public void blood() {
 		System.out.println("message of team lead");
 		System.out.println("todays message from the bloodfort");
 	}
